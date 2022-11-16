2690796ec16061f3919b9f85db1ff4cd66a830f7
shmuto <dos9954@gmail.com>
2022-11-16 23:38:43 +0900

    add config file reading

--- main.go
+++ main.go
@@ -1,30 +1,48 @@
 package main
 
 import (
+	"io/ioutil"
 	"net"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog/log"
 	"github.com/tcaine/twamp"
+	"gopkg.in/yaml.v2"
 )
 
+type Config struct {
+	ControlPort  int `yaml:"controlPort"`
+	SenderPort   int `yaml:"senderPort"`
+	ReceiverPort int `yaml:"receiverPort"`
+	Count        int `yaml:"count"`
+	Timeout      int `yaml:"timeout"`
+}
+
 func main() {
+
+	log.Print("loading configuration file")
+	// load config
+	configFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		log.Print("no configuration file found")
+		os.Exit(1)
+	}
+
+	log.Print("parsing configuration")
+	var config Config
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		log.Print("configuration parse failed")
+		os.Exit(1)
+	}
+	log.Print(config)
+
 	http.HandleFunc("/probe", func(w http.ResponseWriter, r *http.Request) {
 
-		twampMinRTTGauge := prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "twamp_min_rtt",
-			Help: "TWAMP Minimum RTT",
-		})
-		twampMaxRTTGauge := prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "twamp_max_rtt",
-			Help: "TWAMP Maximum RTT",
-		})
-		twampAvgRTTGauge := prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "twamp_avg_rtt",
-			Help: "TWAMP Average RTT",
-		})
 		twampSuccessGauge := prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "twamp_success",
 			Help: "TWAMP sucess or not",
@@ -41,11 +59,13 @@ func main() {
 		registry := prometheus.NewRegistry()
 		registry.MustRegister(twampSuccessGauge)
 
+		twampServerIPandPort := targetIP.String() + ":" + strconv.Itoa(config.ControlPort)
+
 		// connect twamp server
 		c := twamp.NewClient()
-		connection, err := c.Connect(targetIP.String() + ":862")
+		connection, err := c.Connect(twampServerIPandPort)
 		if err != nil {
-			log.Print("Connection failed to " + targetIP.String() + ":862")
+			log.Print("Connection failed to " + twampServerIPandPort)
 			twampSuccessGauge.Set(0)
 			h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
 			h.ServeHTTP(w, r)
@@ -54,15 +74,15 @@ func main() {
 
 		session, err := connection.CreateSession(
 			twamp.TwampSessionConfig{
-				ReceiverPort: 19000,
-				SenderPort:   19000,
-				Timeout:      1,
-				Padding:      100,
+				ReceiverPort: config.ReceiverPort,
+				SenderPort:   config.SenderPort,
+				Timeout:      config.Timeout,
+				Padding:      config.Count,
 				TOS:          0,
 			},
 		)
 		if err != nil {
-			log.Print("could not establishe the session")
+			log.Print("failed to create session")
 			twampSuccessGauge.Set(0)
 			h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
 			h.ServeHTTP(w, r)
@@ -71,19 +91,31 @@ func main() {
 
 		test, err := session.CreateTest()
 		if err != nil {
-			log.Print("coould not create the test")
+			log.Print("failed to create test")
 			twampSuccessGauge.Set(0)
 			h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
 			h.ServeHTTP(w, r)
 			return
 		}
 
-		count := 100
-		results := test.RunX(count, func(result *twamp.TwampResults) {
+		results := test.RunX(config.Count, func(result *twamp.TwampResults) {
 		})
 
 		session.Stop()
 		connection.Close()
+
+		twampMinRTTGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "twamp_min_rtt",
+			Help: "TWAMP Minimum RTT",
+		})
+		twampMaxRTTGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "twamp_max_rtt",
+			Help: "TWAMP Maximum RTT",
+		})
+		twampAvgRTTGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "twamp_avg_rtt",
+			Help: "TWAMP Average RTT",
+		})
 
 		registry.MustRegister(twampMinRTTGauge)
 		registry.MustRegister(twampMaxRTTGauge)
